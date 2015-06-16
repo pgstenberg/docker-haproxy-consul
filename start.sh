928fb584cb9a22bf2fce7aa506c6b9c21472ea8c
Per-Gustaf Stenberg <per-gustaf.stenberg@stonepath.se>
2015-06-16 16:30:00 +0200

    added possability to mount config-file

--- start.sh
+++ start.sh
@@ -4,7 +4,9 @@ echo "Setting up proxy agains consul-url ${CONSUL_URL}."
 echo "Binding configuration with ${CONSUL_CONFIG_BINDING}"
 
 echo "Generating initial HAProxy configuration..."
+cat ${CONSUL_CONFIG_BINDING_TEMPLATE}
 consul-template -consul ${CONSUL_URL} -template ${CONSUL_CONFIG_BINDING} -once
+cat ${CONSUL_CONFIG_BINDING_CONFIG}
 
 echo "Starting HAProxy..."
 haproxy -f ${CONSUL_CONFIG_BINDING_CONFIG} -p /var/run/haproxy.pid -D
